508ac0186176b0414e4fc3850e13cb21bb8eca7d
Nitai Charan <nitaicharan@gmail.com>
2020-01-08 23:10:35 -0300

    132. Stateful Widget Lifecycle Methods
    - initState
    - deactivete

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -7,23 +7,20 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   void getLocation() async {
     Position position = await Geolocator()
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-        print(position);
+    print(position);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
